124b748afa8b721476ffa116a4d89b4a5e31dcce
Miguel Santos <migueljsantos27@gmail.com>
2020-09-30 12:17:35 +0200

    new fils contact form

---
+++ script.js
@@ -0,0 +1,15 @@
+ 
+document 
+
+  .getElementById("contactForm") 
+
+  .addEventListener("submit", function(event) { 
+
+    event.preventDefault(); 
+
+    alert(message); 
+
+  }); 
+  const message = 
+
+  "Thank you for submitting your request."; 
