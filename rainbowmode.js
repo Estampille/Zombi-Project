fc599aed8afe4b1b38a72ee2f527c07cf5c1c8a5
Valerie TV <vtylskivincent@gmail.com>
2020-10-08 17:31:46 +0200

    improve rainbow mode

--- rainbowmode.js
+++ rainbowmode.js
@@ -4,10 +4,23 @@ rainbowModeBtn.addEventListener(
   'click',
   () => {
     document.body.classList.toggle('rainbow');
+
+    const allTheMobileButtons = document.querySelectorAll('#categories article div a div');
+    const allTheDesktopButtons = document.querySelectorAll('#categories article div h3 a span');
+    for(i=0; i<allTheMobileButtons.length; i++){
+      allTheMobileButtons[i].classList.toggle('rainbow-button');
+    }
+    for(i=0; i<allTheDesktopButtons.length; i++){
+      allTheDesktopButtons[i].classList.toggle('rainbow-button');
+    }
+
     document.querySelector('footer').classList.toggle('rainbow-footer');
+    document.querySelector('.navbar ul').classList.toggle('rainbow-footer');
+
     if(document.body.classList == 'rainbow'){
-      console.log("rainbow mode");
-      rainbowModeBtn.innerText = "Repasser en mode réel";
-    } else { rainbowModeBtn.innerText = "Voir la vie en rose"; }
+      rainbowModeBtn.innerText = "Retour à la réalité";
+    } else { 
+      rainbowModeBtn.innerText = "Voir la vie en rose"; 
+    }
   }
 );
